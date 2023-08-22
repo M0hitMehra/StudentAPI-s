83df6d848f370e67f15ddec69dbdf8ec02fc6135
Mohit Mehra <98013205+M0hitMehra@users.noreply.github.com>
2023-08-22 20:45:07 +0530

    2nd

--- server.js
+++ server.js
@@ -6,9 +6,47 @@ const port = 3000;
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.status(200).send(" <h1>Welcome!</h1> <br/> <p> Api is under development");
-});
+
+app.get('/', (req, res) => {
+    const html = `
+      <html>
+      <head>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+          }
+          .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+          }
+          .link {
+            display: block;
+            margin: 10px 0;
+            padding: 10px;
+            border: 1px solid #ccc;
+            background-color: #f9f9f9;
+            text-decoration: none;
+            color: #333;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <h1>Welcome!</h1>
+          <p>API is under development.</p>
+          <p>Click on the links below to access different routes:</p>
+          <a class="link" href="/api">API Home</a>
+          <a class="link" href="/api/1">Get Student by ID</a>
+          <a class="link" href="/api">Add Student</a>
+          <a class="link" href="/api/1">Delete Student</a>
+          <a class="link" href="/api/1">Update Student</a>
+        </div>
+      </body>
+      </html>
+    `;
+    res.status(200).send(html);
+  });
 
 app.use("/api/v1/students", studentRoutes);
 
